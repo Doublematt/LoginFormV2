d4966eec99ed8538d801a7e4cf66aedce0c438db
Mateusz Matusewicz <mateusz.matusewicz16@gmail.com>
2022-08-29 23:39:47 +0200

    files and design prepare files, .css .fxml .java, design login form

--- src/main/java/com/example/loginform/Main.java
+++ src/main/java/com/example/loginform/Main.java
@@ -7,12 +7,12 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("MainStyles.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Login!");
+        stage.setTitle("Login Form");
         stage.setScene(scene);
         stage.show();
     }
