a843c5b6010e042ed19a88acea0f9829c2e629cc
Mateusz Matusewicz <mateusz.matusewicz16@gmail.com>
2022-08-30 07:43:39 +0200

    adding logged in screen

--- src/main/java/com/example/loginform/MainController.java
+++ src/main/java/com/example/loginform/MainController.java
@@ -1,5 +1,49 @@
 package com.example.loginform;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class MainController {
+
+    @FXML
+    private Button loginButton;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private TextField usernameField;
+
+    public void login (ActionEvent event){
+
+        try {
+//            Parent root = FXMLLoader.load(getClass().getResource("loggedinScene.fxml"));
+//            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+//            Scene scene = new Scene(root);
+//            stage.setScene(scene);
+//            stage.show();
+
+            FXMLLoader loader = new FXMLLoader(Main.class.getResource("loggedinScene.fxml"));
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            Parent root = loader.load();
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+
+        }catch(IOException e){
+            System.out.println(" Error: " + e.getMessage());
+
+        }
+    }
 
 }
---
+++ src/main/java/com/example/loginform/loggedinController.java
@@ -0,0 +1,5 @@
+package com.example.loginform;
+
+public class loggedinController {
+
+}
