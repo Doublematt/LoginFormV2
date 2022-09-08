e11f102ee001f32ec07a8d19f816ef91789f52aa
Mateusz Matusewicz <mateusz.matusewicz16@gmail.com>
2022-09-08 21:31:18 +0200

    after login scene created successfully logged in scene and animated it created exit button on that scene and handled exit/close operation

--- src/main/java/com/example/loginform/loggedinController.java
+++ src/main/java/com/example/loginform/loggedinController.java
@@ -1,5 +1,44 @@
 package com.example.loginform;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
 public class loggedinController {
+
+    @FXML
+    private Button exitButton;
+
+    @FXML
+    private AnchorPane mainPanel;
+
+
+    Stage stage;
+    Alert alert;
+
+
+    /*
+  creates an Alert confirmation instance
+  waits for user answer and if its OK then closes the program
+*/
+    public void exit(ActionEvent event) {
+
+        //alert instance
+        alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Exit");
+        alert.setHeaderText("");
+        alert.setContentText("Are you sure? ");
+
+        //checking if user wants to exit
+        if (alert.showAndWait().get() == ButtonType.OK) {
+            stage = (Stage) mainPanel.getScene().getWindow();
+            System.out.println("Successfully exited");
+            stage.close();
+        }
+    }
 
 }
